18f1cdd4e938529d53bedd8c973a69c7f7d01da6
danaivach <danai.vacht@gmail.com>
2023-09-20 11:44:05 +0200

    fix(rsc-profile) dependency to hmas-java

--- src/env/hmas/ResourceArtifact.java
+++ src/env/hmas/ResourceArtifact.java
@@ -1,4 +1,11 @@
 package hmas;
 
-public class ResourceArtifact {
+import cartago.Artifact;
+import ch.unisg.ics.interactions.hmas.core.hostables.ResourceProfile;
+
+public class ResourceArtifact extends Artifact {
+
+  protected ResourceProfile profile;
+
+  
 }
